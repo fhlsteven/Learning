9a382859a411b23c78ea144981f16edb2b90184c
fhlsteven <1508727061@qq.com>
2025-11-30 14:31:46 +0800

    delete jangshi imgs

--- 6_Python/jiangshi/play_normal.py
+++ 6_Python/jiangshi/play_normal.py
@@ -9,24 +9,23 @@ class PlayNormal(Base):
 
     def playNormal(self):
         print('playNormal start')
-        startplay = self.get_pos_byimg("start.png", (0,0), 0.7)
-        print(startplay)
-        if startplay and startplay[0] > 0:
-            self.click_by_js(startplay[0], [1])
-            wait_time(2)
-        else:
-            print('palynormal not found start img')
+        
+        #startplay = self.get_pos_byimg("start.png", (0,0), 0.7)
+        #print(startplay)
+        #if startplay and startplay[0] > 0:
+        #    self.click_by_js(startplay[0], [1])
+        #    wait_time(2)
+        #else:
+        #    print('palynormal not found start img')
         
         while True:
             if self.is_exists_image("select.png", confidence=0.7):
                 self.click_pos((255, 835))
-                wait_time(5)
                 print('playNormal select')
             elif self.is_exists_image("replay.png",confidence=0.7):
                 print('replay select')
                 self.click_pos((250, 1040))
-                wait_time(10)
-            wait_time(2)
+            wait_time(3)
 
     def test_click_pos(self, x,y):
         self.click_pos((x,y))
